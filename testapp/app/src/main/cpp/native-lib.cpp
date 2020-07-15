0ac52211227935a6341e65700fcdb3c089b66451
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-16 00:24:25 +0300

    added function to indentify chessboard

--- testapp/app/src/main/cpp/native-lib.cpp
+++ testapp/app/src/main/cpp/native-lib.cpp
@@ -2,25 +2,40 @@
 #include <android/log.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
-#define TAG "NativeLib"
+#include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
 using namespace cv;
 
 extern "C" {
     void JNICALL
-    Java_com_example_testapp_CvCameraViewListener_adaptiveThresholdFromJNI(
-            JNIEnv *env,jobject instance,jlong matAddr) {
+    Java_com_example_testapp_CvCameraViewListener_identifyChessboard(
+            JNIEnv *env,jobject instance,jlong matAddr,
+            jint boardWidth, jint boardHeight) {
 
-        Mat &mat = *(Mat *) matAddr;
+        vector<Point2f> corners;
 
-        clock_t begin = clock();
+        Mat &frame = *(Mat *) matAddr;
 
-        cv::adaptiveThreshold(mat, mat, 170, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 21, 5);
+        Size patternSize(boardWidth,boardHeight);
 
-        double totalTime = double(clock() - begin) / CLOCKS_PER_SEC;
-        __android_log_print(ANDROID_LOG_INFO, TAG, "adaptiveThreshold computation time = %f seconds\n",
-        totalTime);
+        Mat gray;
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+
+        bool patternFound = findChessboardCorners(gray, patternSize, corners,
+                CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
+
+        if(patternFound) {
+            cornerSubPix(gray, corners, Size(11, 11),
+                         Size(-1, -1),
+                         TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 30, 0.1));
+        }
+        drawChessboardCorners(frame, patternSize, Mat(corners), patternFound);
     }
 }
