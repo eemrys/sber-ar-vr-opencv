034e731b0a97c30fd35f6ca6e5f82b9845b6f276
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-16 21:58:16 +0300

    created a parcelable class for results

--- testapp/app/src/main/java/com/example/testapp/CameraInfo.kt
+++ testapp/app/src/main/java/com/example/testapp/CameraInfo.kt
@@ -1,7 +1,13 @@
 package com.example.testapp
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import org.opencv.core.Mat
+
 data class CameraInfo(
-    var matrix: String = "",
-    var dist: String = "",
-    var rvecs: String = "",
-    var tvecs: String = "")
+    var matrix: Mat = Mat(),
+    var dist: Mat = Mat())
+
+@Parcelize data class CameraInfoString(
+    var matrix: String,
+    var dist: String) : Parcelable
