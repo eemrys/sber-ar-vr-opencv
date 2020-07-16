fe70209afb05d378505524fd49ee9885cd1b928e
eemrys <49338231+eemrys@users.noreply.github.com>
2020-07-16 21:56:24 +0300

    replaced activity with fragment

---
+++ testapp/app/src/main/java/com/example/testapp/CameraFragment.kt
@@ -0,0 +1,74 @@
+package com.example.testapp
+
+import android.os.Bundle
+import android.view.*
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_camera.*
+import org.opencv.android.OpenCVLoader
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+    private val camera by lazy {
+        CvCameraViewListener
+    }
+
+    private val navOptions by lazy {
+        NavOptions.Builder().setEnterAnim(R.anim.slide_in_left)
+            .setPopEnterAnim(R.anim.slide_in_left).build()
+    }
+
+    init {
+        initOpenCv()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setOnClickListeners()
+
+        main_surface.apply {
+            visibility = SurfaceView.VISIBLE
+            setCvCameraViewListener(camera)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        main_surface.enableView()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        main_surface.disableView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        main_surface.disableView()
+    }
+
+    private fun initOpenCv() {
+        val isLoadOpenCVSuccess = OpenCVLoader.initDebug()
+        if(isLoadOpenCVSuccess) initMyNativeLib()
+    }
+
+    private fun initMyNativeLib() {
+        System.loadLibrary("native-lib")
+    }
+
+    private fun setOnClickListeners() {
+        btnCalibrate.setOnClickListener {
+            val results = camera.calibrateCamera()
+            navigateToResults(results)
+        }
+    }
+
+    private fun navigateToResults(results: CameraInfo) {
+        val args = CameraInfoString(results.matrix.dump(), results.dist.dump())
+        val bundle = bundleOf("results" to args)
+        findNavController().navigate(R.id.fragmentResults, bundle, navOptions)
+    }
+}
--- testapp/app/src/main/java/com/example/testapp/MainActivity.kt
+++ testapp/app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,26 +4,14 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.SurfaceView
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import org.opencv.android.OpenCVLoader
+import kotlinx.android.synthetic.main.fragment_camera.*
 
-private const val TAG = "MainActivity"
 private const val CAMERA_PERMISSION_REQUEST = 1
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    private val camera by lazy {
-        CvCameraViewListener
-    }
-
-    init {
-        initOpenCv()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,30 +23,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             arrayOf(Manifest.permission.CAMERA),
             CAMERA_PERMISSION_REQUEST
         )
-
-        setOnClickListeners()
-
-        main_surface.apply {
-            visibility = SurfaceView.VISIBLE
-            setCvCameraViewListener(camera)
-        }
-
-        btnClear.isEnabled = false
-    }
-
-    override fun onResume() {
-        super.onResume()
-        main_surface.enableView()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        main_surface.disableView()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        main_surface.disableView()
     }
 
     override fun onRequestPermissionsResult(
@@ -73,47 +37,13 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                     main_surface.setCameraPermissionGranted()
                 } else {
                     val message = "Camera permission was not granted"
-                    Log.e(TAG, message)
                     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
                 }
             }
             else -> {
-                Log.e(TAG, "Unexpected permission request")
+                val message = "Unexpected permission request"
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
             }
-        }
-    }
-
-    private fun initOpenCv(){
-        val isLoadOpenCVSuccess = OpenCVLoader.initDebug()
-        if(isLoadOpenCVSuccess) initMyNativeLib()
-        Log.d(TAG, "isLoadOpenCVSuccess: $isLoadOpenCVSuccess")
-    }
-
-    private fun initMyNativeLib(){
-        System.loadLibrary("native-lib")
-    }
-
-    private fun setOnClickListeners() {
-        btnResults.setOnClickListener {
-            main_surface.disableView()
-            updateData()
-            it.isEnabled = false
-            btnClear.isEnabled = true
-        }
-        btnClear.setOnClickListener {
-            main_surface.enableView()
-            updateData()
-            it.isEnabled = false
-            btnResults.isEnabled = true
-        }
-    }
-
-    private fun updateData() {
-        camera.cameraInfo.apply {
-            txtvMatrixRes.text = matrix
-            txtvDistRes.text = dist
-            txtvRvecsRes.text = rvecs
-            txtvTvecsRes.text = tvecs
         }
     }
 }
